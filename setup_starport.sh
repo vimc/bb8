7468e541cbc5f3ba0adba946d4713e31b91941db
Alex Hill <alex.hill@gmail.com>
2018-01-24 14:52:54 +0000

    wip

--- setup_starport.sh
+++ setup_starport.sh
@@ -6,11 +6,6 @@ source ${BASH_SOURCE%/*}/vault_auth.sh
 KEY_PATH=ssh_key
 
 mkdir -p $KEY_PATH
-ssh-keygen -f $KEY_PATH/id_rsa -q -N ""
-vault write secret/annex/id_rsa value=@$KEY_PATH/id_rsa
-vault write secret/annex/id_rsa.pub value=@$KEY_PATH/id_rsa.pub
-
-cat $KEY_PATH/id_rsa.pub >> ~/.ssh/authorized_keys
 
 function cleanup {
     set +e
@@ -18,5 +13,10 @@ function cleanup {
 }
 trap cleanup EXIT
 
+ssh-keygen -f $KEY_PATH/id_rsa -q -N ""
+vault write secret/annex/id_rsa value=@$KEY_PATH/id_rsa
+vault write secret/annex/id_rsa.pub value=@$KEY_PATH/id_rsa.pub
+
+cat $KEY_PATH/id_rsa.pub >> ~/.ssh/authorized_keys
 
 echo "Setup of Starport complete. You can now set up production"
--- targets.py
+++ targets.py
@@ -1,5 +1,7 @@
 from os import makedirs
 from os.path import isdir
+from subprocess import run, PIPE
+
 
 class DirectoryTarget:
     def __init__(self, path):
@@ -13,3 +15,25 @@ class DirectoryTarget:
         if not isdir(self.path):
             print("Creating empty directory " + self.path)
             makedirs(self.path)
+
+
+class NamedVolumeTarget:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def id(self):
+        return "Named volume: " + self.name
+
+    def _volume_exists(self):
+        text = run(
+            ["docker", "volume", "ls", "-q"],
+            stdout=PIPE, universal_newlines=True
+        ).stdout
+        names = text.split('\n')
+        return self.name in names
+
+    def before_restore(self):
+        if not self._volume_exists():
+            print("Creating docker volume with name '{}'".format(self.name))
+            run(["docker", "volume", "create", "--name", self.name], stdout=PIPE)
