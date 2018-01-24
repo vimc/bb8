94e32113592003f6184c9c6cefb8469d522bb2c3
Alex Hill <alex.hill@gmail.com>
2018-01-24 13:27:48 +0000

    pull out vault auth intoscript

--- setup.sh
+++ setup.sh
@@ -3,16 +3,7 @@ set -e
 
 #apt-get install python3-pip -y
 
-#source
-
-export VAULT_ADDR='https://support.montagu.dide.ic.ac.uk:8200'
-if [ "$VAULT_AUTH_GITHUB_TOKEN" = "" ]; then
-    echo -n "Please provide your GitHub personal access token for the vault: "
-    read -s token
-    echo ""
-    export VAULT_AUTH_GITHUB_TOKEN=${token}
-fi
-vault auth --method=github
+source vault_auth.sh
 
 ${BASH_SOURCE%/*}/setup.py
 
--- setup_starport.sh
+++ setup_starport.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export VAULT_ADDR='https://support.montagu.dide.ic.ac.uk:8200'
-if [ "$VAULT_AUTH_GITHUB_TOKEN" = "" ]; then
-    echo -n "Please provide your GitHub personal access token for the vault: "
-    read -s token
-    echo ""
-    export VAULT_AUTH_GITHUB_TOKEN=${token}
-fi
-vault auth --method=github
+source vault_auth.sh
 
 KEY_PATH=ssh_key
 
