14aa8012aaf15ccffd0d328e84af2935d75f73a0
Alex Hill <alex.hill@gmail.com>
2018-01-24 17:29:00 +0000

    save contents of key file, not location!

--- setup_starport.sh
+++ setup_starport.sh
@@ -16,7 +16,7 @@ trap cleanup EXIT
 ssh-keygen -f $KEY_PATH/id_rsa -q -N ""
 vault write secret/annex/id_rsa value=@$KEY_PATH/id_rsa
 vault write secret/annex/id_rsa.pub value=@$KEY_PATH/id_rsa.pub
-vault write secret/annex/host_key value=/etc/ssh/ssh_host_rsa_key.pub
+vault write secret/annex/host_key value=@/etc/ssh/ssh_host_rsa_key.pub
 
 cat $KEY_PATH/id_rsa.pub >> ~/.ssh/authorized_keys
 
