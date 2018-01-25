87ef325b544ef5bfb1a8ca528b6d4acaeaae3b73
Martin Eden <martin.s.eden@gmail.com>
2018-01-25 12:38:46 +0000

    Move config for user

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
-from os.path import isfile
+from os.path import isfile, isdir, join
+from os import listdir, mkdir
+from shutil import copy
 
 from settings import config_path, root_path, save_private_key
 
+
+def get_user_config_choice():
+    options = listdir(configs_source)
+    prompt = "Please choose which config to use ({})? ".format(", ".join(options))
+    choice = None
+    path = None
+    while not choice:
+        choice = input(prompt).strip()
+        if choice:
+            path = join(configs_source, choice, 'config.json')
+            if not isfile(path):
+                choice = None
+                print("'{}' does not exist".format(path))
+    return path
+
+
 if __name__ == "__main__":
+    if not isdir(root_path):
+        mkdir(root_path)
     if not isfile(config_path):
-        print("Missing config files in {}. First copy files from configs/".format(root_path))
-        print("For example, for setting up backups on the support machine:")
-        print("mkdir -p {0} && cp configs/support.montagu/* {0}".format(root_path))
-        exit(-1)
+        configs_source = './configs'
+        print("Missing config files in {}.".format(root_path))
+        path = get_user_config_choice()
+        print("Copying {} to {}".format(path, root_path))
+        copy(path, root_path)
 
     print("Obtaining secrets from the vault. If you are not authenticated with the vault, this will fail.")
     save_private_key()
