bb33871e8d749be4b5e29adf741eb8ee2f1a98a8
Alex Hill <alex.hill@gmail.com>
2018-02-02 14:33:05 +0000

    keep code DRY

--- docker_rsync.py
+++ docker_rsync.py
@@ -7,21 +7,16 @@ from logger import log_from_docker
 client = docker.from_env()
 
 
-# target_volume can be an absolute path or a named volume
-def backup_volume(settings, local_volume, starport):
-    mounted_volume = join("/", local_volume)
-    volumes = {abspath(settings.known_hosts_path): {"bind": "/root/.ssh/known_hosts", "mode": "rw"},
-               abspath(settings.ssh_key_path): {"bind": "/root/.ssh/id_rsa", "mode": "ro"},
-               local_volume: {"bind": "{}".format(mounted_volume), "mode": "ro"}}
-
+def run_rsync(volumes, from_path, to_path, relative):
     # rsync options:
     # -v = verbose - give info about what files are being transferred and a brief summary at the end
     # -r = copy directories recursively
     # -e = specify remote shell program explicitly (i.e. ssh as opposed to the default rsh)
-    destination_path = "{}@{}:{}".format(starport["user"],
-                                         starport["addr"],
-                                         starport["backup_location"])
-    cmd = ["rsync", "-rv", "-e", "ssh", "--relative", mounted_volume, destination_path]
+    # --delete = delete destination files not in source
+    cmd = ["rsync", "-rv", "-e", "ssh", from_path, to_path, "--delete"]
+
+    if relative:
+        cmd = ["rsync", "-rv", "-e", "ssh", "--relative", from_path, to_path, "--delete"]
 
     container = client.containers.run("instrumentisto/rsync-ssh", command=cmd, volumes=volumes,
                                       detach=True)
@@ -29,18 +24,31 @@ def backup_volume(settings, local_volume, starport):
     log_from_docker(container)
 
 
+def create_volumes(settings, local_volume, volume_mode):
+    mounted_volume = join("/", local_volume)
+    return {abspath(settings.known_hosts_path): {"bind": "/root/.ssh/known_hosts", "mode": "rw"},
+            abspath(settings.ssh_key_path): {"bind": "/root/.ssh/id_rsa", "mode": "ro"},
+            local_volume: {"bind": "{}".format(mounted_volume), "mode": volume_mode}}
+
+
+# local_volume can be an absolute path or a named volume
+def backup_volume(settings, local_volume, starport):
+    volumes = create_volumes(settings, local_volume, "ro")
+
+    destination_path = "{}@{}:{}".format(starport["user"],
+                                         starport["addr"],
+                                         starport["backup_location"])
+
+    run_rsync(volumes, local_volume, destination_path, True)
+
+
 def restore_volume(settings, local_volume, starport):
     mounted_volume = join("/", local_volume)
-    volumes = {abspath(settings.known_hosts_path): {"bind": "/root/.ssh/known_hosts", "mode": "rw"},
-               abspath(settings.ssh_key_path): {"bind": "/root/.ssh/id_rsa", "mode": "ro"},
-               local_volume: {"bind": "{}".format(mounted_volume), "mode": "rw"}}
+    volumes = create_volumes(settings, local_volume, "rw")
 
     remote_path = "{}@{}:{}{}/".format(starport["user"],
                                        starport["addr"],
                                        starport['backup_location'],
                                        local_volume)
-    cmd = ["rsync", "-rv", "-e", "ssh", remote_path, mounted_volume]
-    container = client.containers.run("instrumentisto/rsync-ssh", command=cmd, volumes=volumes,
-                                      detach=True)
 
-    log_from_docker(container)
+    run_rsync(volumes, remote_path, mounted_volume, False)
