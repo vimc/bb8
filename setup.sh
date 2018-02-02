6b032654c0888880de15e7d220891d463c14381e
Alex Hill <alex.hill@gmail.com>
2018-02-02 10:42:29 +0000

    amendments from Rich

--- setup.sh
+++ setup.sh
@@ -1,42 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-source ${BASH_SOURCE%/*}/vault_auth.sh
+HERE=${BASH_SOURCE%/*}
+source ${HERE}/vault_auth.sh
 
 echo "Installing Pip and required Python packages"
 apt-get install python3-pip -y > /dev/null
-pip3 install -q -r requirements.txt
+pip3 install -q -r ${HERE}/requirements.txt
 
 echo "-------------------------------------------"
 echo "Installing bb8 user and group"
 
 if ! id -u bb8 > /dev/null 2>&1; then
-    useradd bb8
+    useradd bb8 -d /usr/lib/bb8
     password=$(vault read --field password secret/backup/bb8/user)
     echo "bb8:$password" | chpasswd
 
-    # set bb8 home dir
-    mkdir /var/lib/bb8/.ssh
-    usermod -m -d /var/lib/bb8 bb8
-    chown -R bb8:bb8 /var/lib/bb8
+    # Perhaps defer until python?
+    mkdir -p /var/lib/bb8/.ssh
 
     # add to docker group
     usermod -aG docker bb8
-fi
-
-if ! /usr/bin/getent group bb8 2>&1 > /dev/null; then
-    groupadd bb8
 fi
 
 # add bb8 user to bb8 group
 usermod -aG bb8 bb8
 
 # give bb8 group owernship of this dir
-chgrp -R bb8 .
+chgrp -R bb8 $HERE
 
 echo "-------------------------------------------"
 echo "Running setup.py:"
-su -c "source ${BASH_SOURCE%/*}/vault_auth.sh && ${BASH_SOURCE%/*}/setup.py" bb8
+(cd "$HERE" && su -c ./setup.py bb8)
 
 echo "-------------------------------------------"
 
