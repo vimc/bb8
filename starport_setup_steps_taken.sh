c7eebac7e532f84b53db9573e814c192c22e722e
Martin Eden <martin.s.eden@gmail.com>
2018-02-16 17:36:44 +0000

    More steps

--- starport_setup_steps_taken.sh
+++ starport_setup_steps_taken.sh
@@ -1,3 +1,4 @@
+# Starport
 ssh annex.montagu
 git clone https://github.com/vimc/bb8
 sudo useradd bb8 -U -d /var/lib/bb8
@@ -8,3 +9,17 @@ cd ~
 mkdir .ssh
 /home/martin/bb8/setup_starport.sh
 mkdir starport
+
+# Support
+# (Upgraded Vault 0.7.3 -> 0.9.3)
+sudo ./setup.sh
+
+Then:
+
+martin@fi--didevimc02:~/bb8$ ./backup.sh 
+Backing up targets to Starport. Output will be logged to ./log/
+INFO    Backing up to annex.montagu.dide.ic.ac.uk: 
+INFO    - Named volume: registry_data
+ERROR    Permission denied (publickey).
+ERROR    rsync: connection unexpectedly closed (0 bytes received so far) [sender]
+ERROR    rsync error: unexplained error (code 255) at io.c(226) [sender=3.1.2]
