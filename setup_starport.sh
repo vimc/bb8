27085ac59d32f8ec11773a5c795d47a055ede35f
Alex Hill <alex.hill@gmail.com>
2018-03-02 11:03:47 +0000

    take out config from json, create bb8 user and starport in setup script

--- setup_starport.sh
+++ setup_starport.sh
@@ -3,6 +3,19 @@ set -e
 
 source ${BASH_SOURCE%/*}/vault_auth.sh
 
+echo "-------------------------------------------"
+echo "Installing bb8 user and group"
+
+if ! id -u bb8 > /dev/null 2>&1; then
+    useradd bb8 -U -d /var/lib/bb8
+    password=$(vault read --field password secret/backup/bb8/user)
+    echo "bb8:$password" | chpasswd
+
+    mkdir -p /var/lib/bb8/.ssh
+fi
+
+mkdir -p /var/lib/bb8/starport
+
 KEY_PATH=ssh_key
 
 mkdir -p $KEY_PATH
@@ -11,6 +24,7 @@ function cleanup {
     set +e
     rm -r $KEY_PATH
 }
+
 trap cleanup EXIT
 
 ssh-keygen -f $KEY_PATH/id_rsa -q -N ""
@@ -18,6 +32,6 @@ vault write secret/annex/id_rsa value=@$KEY_PATH/id_rsa
 vault write secret/annex/id_rsa.pub value=@$KEY_PATH/id_rsa.pub
 vault write secret/annex/host_key value=@/etc/ssh/ssh_host_ecdsa_key.pub
 
-cat $KEY_PATH/id_rsa.pub >> ~/.ssh/authorized_keys
+cat $KEY_PATH/id_rsa.pub >> /var/lib/bb8/.ssh/authorized_keys
 
 echo "Setup of Starport complete. You can now set up production"
