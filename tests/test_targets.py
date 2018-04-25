940b64e3d21952813c2d7f3d93ca15c4ac10b4e6
Martin Eden <martin.s.eden@gmail.com>
2018-04-25 15:31:20 +0100

    Upgrade tests to pytest

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,41 +1,42 @@
-import unittest
-
 import docker
 
 from bin import targets
 
 
-class TestNamedVolumeTarget(unittest.TestCase):
+# noinspection PyProtectedMember
+class TestNamedVolumeTarget(object):
     """Tests for NamedVolumeTarget"""
     docker = docker.client.from_env()
+    dummy_volume_name = "bb8_test_volume"
 
-    def setUp(self):
-        self._dummy_volume_name = "bb8_test_volume"
-        if not self.docker.ping():
-            self.skipTest("docker is not available")
-        self.tearDown()
+    @classmethod
+    def setup_class(cls):
+        cls.remove_test_volume_if_exists()
 
-    def tearDown(self):
-        try:
-            self.docker.volumes.get(self._dummy_volume_name).remove()
-        except:
-            pass
+    @classmethod
+    def teardown_class(cls):
+        cls.remove_test_volume_if_exists()
 
     def test_properties(self):
         """Test properties of constructed volume target"""
-        vol = targets.NamedVolumeTarget("mytarget", self._dummy_volume_name)
-        self.assertEqual(vol.name, "mytarget")
-        self.assertEqual(vol.volume, self._dummy_volume_name)
-        self.assertEqual(vol.id, "Named volume: mytarget")
-        self.assertEqual(vol.mount_id, self._dummy_volume_name)
+        vol = targets.NamedVolumeTarget("mytarget", self.dummy_volume_name, None)
+        assert vol.name == "mytarget"
+        assert vol.volume == self.dummy_volume_name
+        assert vol.id == "Named volume: mytarget"
+        assert vol.mount_id == self.dummy_volume_name
 
     def test_creation(self):
         """Test creation of volume"""
-        vol = targets.NamedVolumeTarget("mytarget", self._dummy_volume_name)
-        self.assertFalse(vol._volume_exists())
+        vol = targets.NamedVolumeTarget("mytarget", self.dummy_volume_name, None)
+        assert not vol._volume_exists()
         vol.before_restore()
-        self.assertTrue(vol._volume_exists())
+        assert vol._volume_exists()
 
-
-if __name__ == '__main__':
-    unittest.main()
+    @classmethod
+    def remove_test_volume_if_exists(self):
+        try:
+            vol = self.docker.volumes.get(self.dummy_volume_name)
+            vol.remove()
+            print("Removed test volume {}".format(self.dummy_volume_name))
+        except docker.errors.NotFound:
+            pass
