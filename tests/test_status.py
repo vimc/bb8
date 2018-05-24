c8ac04988eb76a67d026f41a5110a68545b4ecd3
Martin Eden <martin.s.eden@pm.me>
2018-05-24 13:19:06 +0100

    Fix tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -5,7 +5,7 @@ from bin.bb8.status import interpret_timestamp_output
 
 def test_that_empty_timestamp_is_interpreted_as_missing_data():
     x = interpret_timestamp_output("".encode("utf-8"))
-    assert x == "No files present"
+    assert x is None
 
 
 def test_that_timestamp_is_correctly_parsed_and_zoned():
@@ -13,4 +13,4 @@ def test_that_timestamp_is_correctly_parsed_and_zoned():
     # Going from +2 to -5 timezone
     x = interpret_timestamp_output(input.encode("utf-8"),
                                    timezone=timezone("America/Jamaica"))
-    assert x == "2018-05-18 08:00:00-05:00"
+    assert x.isoformat(" ") == "2018-05-18 08:00:00-05:00"
